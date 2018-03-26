1c877ae50c85da5b6d5ef17d50652b29c6d0407f
akoper <andrew.koper@gmail.com>
2018-03-25 21:38:24 -0400

    added all modules imported up to this point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { UserCreateComponent } from './user-create/user-create.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { TaskCreateComponent } from './task-create/task-create.component';
 import { TaskReadComponent } from './task-read/task-read.component';
 import { TaskShowComponent } from './task-show/task-show.component';
-import {TableModule} from 'primeng/table';
+import { TableModule} from 'primeng/table';
 import { HeroReadComponent } from './hero-read/hero-read.component';
-import {HttpClientModule} from "@angular/common/http";
-import {TaskService} from "./task.service";
+import { HttpClientModule} from "@angular/common/http";
+import { TaskService} from "./task.service";
+import { TaskReadDatatableComponent } from './task-read-datatable/task-read-datatable.component';
+import { TaskReadDatatableUpdateComponent } from './task-read-datatable-update/task-read-datatable-update.component';
+import { TaskUpdateComponent } from './task-update/task-update.component';
+import { TaskDeleteComponent } from './task-delete/task-delete.component';
+import { DataTableModule} from "primeng/primeng";
+import { HttpModule} from "@angular/http";
+
 
 @NgModule({
     declarations: [
@@ -21,7 +28,11 @@ import {TaskService} from "./task.service";
         TaskCreateComponent,
         TaskReadComponent,
         TaskShowComponent,
-        HeroReadComponent
+        HeroReadComponent,
+        TaskReadDatatableComponent,
+        TaskReadDatatableUpdateComponent,
+        TaskUpdateComponent,
+        TaskDeleteComponent,
     ],
     exports: [
     ],
@@ -31,7 +42,9 @@ import {TaskService} from "./task.service";
         ReactiveFormsModule,
         AppRoutingModule,
         TableModule,
-        HttpClientModule
+        HttpClientModule,
+        DataTableModule,
+        HttpModule,
     ],
     providers: [TaskService],
     bootstrap: [AppComponent]
