9ca1337bc1fb2be334dcaf7f9e41a944ec167659
akoper <andrew.koper@gmail.com>
2018-03-25 22:31:45 -0400

    Validation added but visual display not working

--- src/app/task-create/task-create.component.ts
+++ src/app/task-create/task-create.component.ts
@@ -1,33 +1,48 @@
-import {Component, OnInit} from '@angular/core';
-import { FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {Task} from "../task";
-import { TaskService} from "../task.service";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from "@angular/forms";
+import { Task } from "../task";
+import { TaskService } from "../task.service";
 
 @Component({
     selector: 'app-task-create',
     templateUrl: './task-create.component.html',
     styleUrls: ['./task-create.component.css']
 })
+
 export class TaskCreateComponent implements OnInit {
 
-    // submitted = false;
+    constructor(private taskService: TaskService) {}
 
-    constructor(private taskService: TaskService) {
-    }
+    private task:Task;
 
-    addTask(name: string): void {
-        // name = name.trim();
-        console.log(name);
-        if (!name) { return; }
-        this.taskService.addTask({ name } as Task)
-            .subscribe();
-    }
+    taskForm: FormGroup;
 
     ngOnInit() {
+        this.taskForm = new FormGroup({
+            proj_id: new FormControl('', [
+                Validators.min(4),
+                Validators.max(10)
+            ]),
+            name: new FormControl('',
+                Validators.required),
+            description: new FormControl(),
+            start_date: new FormControl('',
+                Validators.required),
+            end_date: new FormControl()
+        })
     }
 
-    // onSubmit() {
-    //     this.submitted = true;
-    // }
+    get proj_id() { return this.taskForm.get('proj_id'); }
+    get name() { return this.taskForm.get('name'); }
+    get description() { return this.taskForm.get('description'); }
+    get start_date() { return this.taskForm.get('start_date'); }
+    get end_date() { return this.taskForm.get('end_date'); }
+
+    onSubmit() {
+        if(this.taskForm.valid) {
+            this.task = this.taskForm.value;
+            this.taskService.addTask(this.task).subscribe(tasks => this.task = tasks);
+        }
+    }
 
 }
